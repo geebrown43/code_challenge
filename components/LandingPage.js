d6e173b3fa262e192107b2a64ff9d2f1119b89a6
Gavin Brown <scareb15@gmail.com>
2018-02-23 21:07:51 -0700

    adjusted alignment on landing page

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -8,14 +8,14 @@ class Landing extends React.Component{
     render(){
         return (
             <View style={{flex: 1, justifyContent:'space-between', margin: 20, borderRadius: 1.5, borderWidth: 0.16, borderColor: 'black', shadowColor: 'black', shadowOffset:{width: 0 , height: 2}, shadowOpacity: 0.2}}>
-                <View style ={{ marginTop: 10, alignItems: 'flex-end'}}>
+                <View style ={{alignItems: 'flex-end'}}>
                     <Button title='Close' onPress={this.props._clearLanding}/>
                 </View>
                 <View style={{alignItems: 'center'}}>
-                    <Image style={{height: 200, width: 350}} source={{uri: this.props.photo[0].urls.regular}}/>
+                    {/* <Image style={{height: 200, width: 310}} source={{uri: this.props.photo[0].urls.regular}}/> */}
                     <View>
-                    <Text>Gavin Brown</Text>
-                    <Text>@gavindevelops</Text>
+                    <Text style={{textAlign: 'center'}}>Gavin Brown</Text>
+                    <Text style={{textAlign: 'center'}}>@gavindevelops</Text>
                     </View>
                 </View>
                 
