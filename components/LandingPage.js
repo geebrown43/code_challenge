ed84accb6eeb8b7e7585c8da71fa97c1f47503c0
Ellis Mwekunda <Scareb15@gmail.com>
2018-02-24 00:12:14 -0700

    close button

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import {View, Text, Button, Image} from 'react-native'
-
+import {View, Text, Image} from 'react-native'
+import {Button, Icon} from 'native-base'
 
 
 
@@ -9,7 +9,10 @@ class Landing extends React.Component{
         return (
             <View style={{flex: 1, justifyContent:'space-between', margin: 20, borderRadius: 1.5, borderWidth: 0.16, borderColor: 'black', shadowColor: 'black', shadowOffset:{width: 0 , height: 2}, shadowOpacity: 0.2}}>
                 <View style ={{alignItems: 'flex-end'}}>
-                    <Button title='Close' onPress={this.props._clearLanding}/>
+                    <View>
+                    <Button transparent large onPress={this.props._clearLanding}><Icon name='close'/></Button>
+                    </View>
+                    
                 </View>
                 <View style={{alignItems: 'center'}}>
                     {/* <Image style={{height: 200, width: 310}} source={{uri: this.props.photo[0].urls.regular}}/> */}
