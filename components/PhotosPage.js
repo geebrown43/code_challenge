5fda78379c63457034373a76cff54616d50cd469
Gavin Brown <scareb15@gmail.com>
2018-02-23 21:26:29 -0700

    added styling for photos page

--- components/PhotosPage.js
+++ components/PhotosPage.js
@@ -1,41 +1,49 @@
 import React from 'react'
-import {View, Text, Button, ScrollView, Image} from 'react-native'
+import { View, Text, Button, ScrollView, Image, TouchableHighlight } from 'react-native'
 
 
-export default class PhotoPage extends React.Component{
-    constructor(){
+export default class PhotoPage extends React.Component {
+    constructor() {
         super()
     }
 
-    render(){
+    render() {
         console.log(this.props.images)
-        return(
-            <View style={{margin:20, justifyContent:'space-around'}}>
-            <View>
+        return (
+            <View style={{ margin: 10, justifyContent: 'space-around' }}>
                 <View>
-                    <Text>My Photos</Text>
-                </View>
-                <View>
-                    {/* Button
+                    <View>
+                        <Text>My Photos</Text>
+                    </View>
+                    <View>
+                        {/* Button
                     Button */}
+                    </View>
                 </View>
-            </View>
-            <View>
-                {/* SearchBar */}
-            </View>
-            <ScrollView>
-                    {this.props.images.map((a, i) =>{
+                <View>
+                    {/* SearchBar */}
+                </View>
+                <ScrollView>
+                    {this.props.images.map((a, i) => {
                         return (
-                            <View key={i} style={{margin: 10}}>
-                            <Image  style={{height:60, width: 90}} source={{uri: a.urls.small }}/>
-                            <Text>{a.links.self}</Text>
+                            <View key={i} style={{ margin: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+                                <View>
+                                    <Image style={{ height: 60, width: 90 }} source={{ uri: a.urls.small }} />
+                                </View>
+
+                                <View style={{justifyContent: 'center', width: 280, margin: 10}}>
+                                    <TouchableHighlight>
+                                    <Text style={{}}>{a.links.self}</Text>
+                                    </TouchableHighlight>
+                                </View>
+
                             </View>
                         )
                     })}
-            </ScrollView>
-            <View style={{height: 60}}>
-                <Text style={{textAlign: 'center'}}>Get Photos</Text>
-            </View>
+                </ScrollView>
+                <View style={{ height: 60 }}>
+                    <Text style={{ textAlign: 'center' }}>Get Photos</Text>
+                </View>
             </View>
         )
     }
