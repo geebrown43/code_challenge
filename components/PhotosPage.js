e772d877e4bef9b2132a6a2a5b9b0e497fe2bddf
Gavin Brown <scareb15@gmail.com>
2018-02-24 02:03:23 -0700

    added function to with component views on photos page

--- components/PhotosPage.js
+++ components/PhotosPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, ScrollView, Image, TouchableHighlight } from 'react-native'
+import { View, Text, ScrollView, Image, TouchableHighlight, StyleSheet } from 'react-native'
 import {Button, Icon} from 'native-base'
 
 
@@ -7,49 +7,56 @@ export default class PhotoPage extends React.Component {
     constructor() {
         super()
         this.state={
-
+            gridView: true,
+            listView: false
         }
+    }
+
+    _switchViews = () => {
+        this.setState({gridView: !this.state.gridView, listView: !this.state.listView})
     }
 
     render() {
         return (
             <View style={{ margin: 10, justifyContent: 'space-around' }}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between', height: 80}}>
+                <View style={styles.headingcontainer}>
                     <View style={{marginTop: 10}}>
-                        <Text style={{fontSize:36}}>My Photos</Text>
+                        <Text style={styles.brand}>My Photos</Text>
                     </View>
                     <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                        <Button iconLeft><Icon name='list'/><Text style={{marginLeft: 5}}>List</Text></Button>
-                        <Button iconLeft><Icon name='grid'/><Text style={{marginLeft: 5}}>Grid</Text></Button>
-                        {/* Button
-                    Button */}
+                        <Button iconLeft onPress={this._switchViews}><Icon name='list'/><Text style={{marginLeft: 5}}>List</Text></Button>
+                        <Button iconLeft onPress={this._switchViews}><Icon name='grid'/><Text style={{marginLeft: 5}}>Grid</Text></Button>
                     </View>
                 </View>
                 <View>
                     {/* SearchBar */}
                 </View>
-                <ScrollView>
-                    {this.props.images.map((a, i) => {
-                        return (
-                            <View key={i} style={{ margin: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-                                <View>
-                                    <Image style={{ height: 60, width: 90 }} source={{ uri: a.urls.small }} />
-                                </View>
-
-                                <View style={{justifyContent: 'center', width: 280, margin: 10}}>
-                                    <TouchableHighlight>
-                                    <Text>{a.links.self}</Text>
-                                    </TouchableHighlight>
-                                </View>
-
-                            </View>
-                        )
-                    })}
-                </ScrollView>
-                <View style={{ height: 60, alignItems: 'center', justifyContent: 'center' }}>
-                    <Text style={{ textAlign: 'center', fontSize: 26 }}>Get Photos</Text>
+                {this.state.gridView}
+                {this.state.listView}
+                <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                    <View>
+                    <Text style={styles.footer}>Get Photos</Text>
+                    </View>
                 </View>
             </View>
         )
     }
 }
+
+
+
+const styles = StyleSheet.create ({
+    brand: {
+        fontFamily: 'open-sans',
+        fontSize: 28
+    },
+    headingcontainer: {
+        flexDirection: 'row', 
+        justifyContent: 'space-between'
+    },
+    footer:{
+        textAlign: 'center',
+        fontSize: 26,
+        fontFamily: 'open-sans'
+    }
+})
