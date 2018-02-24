132fd674064a42fb669d2062562e3693990f8f56
Gavin Brown <scareb15@gmail.com>
2018-02-24 02:02:08 -0700

    added gridview component

---
+++ components/GridView.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import {View, ScrollView, Image} from 'react-native'
+
+
+
+export default class GridView extends React.Component {
+
+    render(){
+        return (
+            <View>
+                <ScrollView>
+                    
+                </ScrollView>
+            </View>
+        )
+    }
+}
