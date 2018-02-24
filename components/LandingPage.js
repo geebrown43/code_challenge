58c25ad44f980d4a10234de3fb8894c5960d0990
Gavin Brown <scareb15@gmail.com>
2018-02-23 20:25:05 -0700

    added styling to landing page

--- components/LandingPage.js
+++ components/LandingPage.js
@@ -7,23 +7,20 @@ import {View, Text, Button, Image} from 'react-native'
 class Landing extends React.Component{
     render(){
         return (
-            <View style={{flex: 1, backgroundColor: 'white'}}>
-                <View style ={{backgroundColor: 'black', marginTop: 30}}>
-                    <Button onPress={this._getphoto} title='Press' />
-                    {/* Button */}
+            <View style={{flex: 1, justifyContent:'space-between', margin: 20, borderRadius: 1.5, borderWidth: 0.16, borderColor: 'black', shadowColor: 'black', shadowOffset:{width: 0 , height: 2}, shadowOpacity: 0.2}}>
+                <View style ={{ marginTop: 10, alignItems: 'flex-end'}}>
+                    <Button title='Close' onPress={this.props._clearLanding}/>
                 </View>
-                <View style={{backgroundColor: 'grey', height: 300}}>
-                    <Image source={{uri: this.props.photos[0].urls.full}}/>
+                <View style={{alignItems: 'center'}}>
+                    <Image style={{height: 200, width: 350}} source={{uri: this.props.photo[0].urls.regular}}/>
+                    <View>
+                    <Text>Gavin Brown</Text>
+                    <Text>@gavindevelops</Text>
+                    </View>
+                </View>
                 
-                {/* Image */}
-                </View>
-                <View>
-                    {/* Personal Image
-                    Name
-                    Handle */}
-                </View>
-                <View>
-                    {/* Location */}
+                <View style={{alignItems: 'center'}}>
+                   <Text>Geocode Location</Text>
                 </View>
             </View>
         )
