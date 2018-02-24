3bb002dc133d54436cfe3a1b4446f18a9b8443c3
Gavin Brown <scareb15@gmail.com>
2018-02-23 20:25:33 -0700

    state control for landing page and function to close page

--- App.js
+++ App.js
@@ -6,18 +6,23 @@ export default class App extends React.Component {
     super()
     this.state ={
       photos: [],
-      landing: true
+      landing: false
     }
   }
   
   async componentDidMount(){
-    fetch('https://api.unsplash.com/photos/random?client_id=5f3c9049f0101b057020dc3171e8b4c01b92b22eb53563e1bbfcf333a44d6bcd&count=10').then(items => items.json()).then(data => this.setState({photos: data}))
+    fetch('https://api.unsplash.com/photos/random?client_id=5f3c9049f0101b057020dc3171e8b4c01b92b22eb53563e1bbfcf333a44d6bcd&count=10').then(items => items.json()).then(data => this.setState({photos: data, landing: !this.state.landing}))
   }
+
+  _clearLanding = () => {
+    this.setState({landing: !this.state.landing})
+  }
+
   render() {
-    console.log(this.state.photos.map(a => a.urls.full))
+    let value = this.state.photos
     return (
-      <View style={{flex : 1, backgroundColor: 'grey'}}>
-        {this.state.landing ? <Landing photo={this.state.photos}/>  : null}
+      <View style={{flex : 1}}>
+        {this.state.landing ? <Landing photo={value} _clearLanding={this._clearLanding}/>  : null}
       </View>
     );
   }
