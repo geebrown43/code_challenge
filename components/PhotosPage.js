3391d7ee89eb3769f842195c2bcb25992d70b328
Gavin Brown <scareb15@gmail.com>
2018-02-24 17:11:16 -0700

    added functions for each view

--- components/PhotosPage.js
+++ components/PhotosPage.js
@@ -1,42 +1,53 @@
 import React from 'react'
 import { View, Text, ScrollView, Image, TouchableHighlight, StyleSheet } from 'react-native'
-import {Button, Icon} from 'native-base'
-
+import { Button, Icon } from 'native-base'
+import Grid from './GridView'
+import List from './ListView'
+import Detail from './DetailsView'
 
 export default class PhotoPage extends React.Component {
-    constructor() {
-        super()
-        this.state={
-            gridView: true,
-            listView: false
-        }
+
+    state = {
+        gridView: true,
+        listView: false,
+        detailsView: false
     }
 
-    _switchViews = () => {
-        this.setState({gridView: !this.state.gridView, listView: !this.state.listView})
+
+    _listView = () => {
+        this.setState({ gridView: false, listView: true, detailsView: false })
+    }
+    _gridView = () => {
+        this.setState({ gridView: true, listView: false, detailsView: false})
+    }
+
+    _detailsView = () => {
+        this.setState({ gridView: false, listView: false, detailsView: true })
     }
 
     render() {
+        console.log(this.props.images)
         return (
             <View style={{ margin: 10, justifyContent: 'space-around' }}>
                 <View style={styles.headingcontainer}>
-                    <View style={{marginTop: 10}}>
+                    <View style={{ marginTop: 10 }}>
                         <Text style={styles.brand}>My Photos</Text>
                     </View>
-                    <View style={{flexDirection: 'row', justifyContent: 'center'}}>
-                        <Button iconLeft onPress={this._switchViews}><Icon name='list'/><Text style={{marginLeft: 5}}>List</Text></Button>
-                        <Button iconLeft onPress={this._switchViews}><Icon name='grid'/><Text style={{marginLeft: 5}}>Grid</Text></Button>
+                    <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                        <Button transparent onPress={this._listView}><Icon name='list' /></Button>
+                        <Button transparent onPress={this._gridView}><Icon name='grid' /></Button>
                     </View>
                 </View>
                 <View>
                     {/* SearchBar */}
                 </View>
-                {this.state.gridView}
-                {this.state.listView}
+                {this.state.gridView ? <Grid images={this.props.images} _detailsView={this._detailsView}/> : null}
+                {this.state.listView ? <List images={this.props.images} _detailsView={this._detailsView}/> : null}
+                {this.state.detailsView ? <Detail images={this.props.images}/>: null}
                 <View style={{ alignItems: 'center', justifyContent: 'center' }}>
                     <View>
-                    <Text style={styles.footer}>Get Photos</Text>
-                    </View>
+                    <Button iconLeft transparent onPress={this.props._newPhotos}><Icon name='ios-refresh-circle'/><Text style={styles.footer}>Get Photos</Text></Button>
+                    </View>                        
                 </View>
             </View>
         )
@@ -45,18 +56,17 @@ export default class PhotoPage extends React.Component {
 
 
 
-const styles = StyleSheet.create ({
+const styles = StyleSheet.create({
     brand: {
         fontFamily: 'open-sans',
         fontSize: 28
     },
     headingcontainer: {
-        flexDirection: 'row', 
+        flexDirection: 'row',
         justifyContent: 'space-between'
     },
-    footer:{
+    footer: {
         textAlign: 'center',
-        fontSize: 26,
-        fontFamily: 'open-sans'
+        fontSize: 18
     }
 })
