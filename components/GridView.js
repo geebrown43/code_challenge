cf4019871952e8d414ae0d97e6793855b0c263dc
Gavin Brown <scareb15@gmail.com>
2018-02-24 17:10:29 -0700

    added styling to gridView

--- components/GridView.js
+++ components/GridView.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import {View, ScrollView, Image} from 'react-native'
+import { ScrollView, Image, View, TouchableHighlight} from 'react-native'
 
 
 
 export default class GridView extends React.Component {
 
-    render(){
+    render() {
+        let photos = this.props.images
         return (
-            <View>
-                <ScrollView>
-                    
-                </ScrollView>
+            <View style={{ flexDirection: 'row', flexWrap: 'wrap', left: 3}}>
+                {photos.map((a, i) => {
+                    return (
+                        <View key={i} style={{marginTop: 15, marginLeft:2, marginRight: 2}}>
+                            <TouchableHighlight onPress={this.props._detailsView}>
+                            <Image style={{height: 105, width: 190}} source={{uri: a.urls.regular }}/> 
+                            </TouchableHighlight>                       
+                        </View>
+                    )
+                }) }
+                
             </View>
+
         )
     }
 }
