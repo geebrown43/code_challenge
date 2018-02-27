9544a6129ef1457811cd25dcad490e6936fe3c98
Gavin Brown <scareb15@gmail.com>
2018-02-27 15:37:15 -0700

    adjusted grid view for multiple grid sizes

--- components/GridView.js
+++ components/GridView.js
@@ -1,26 +1,59 @@
 import React from 'react'
-import { ScrollView, Image, View, TouchableHighlight} from 'react-native'
+import { ScrollView, Image, View, TouchableHighlight, Dimensions, StyleSheet} from 'react-native'
 
 
+
+const {width, height} = Dimensions.get('screen')
 
 export default class GridView extends React.Component {
+    state = {
+        gridSize: styles.smallGrid
+    }
 
+    async componentDidMount(){
+       if(width > 700){
+           return this.setState({gridSize: styles.largeGrid})
+       } else {
+        return this.setState({gridSize:styles.smallGrid})
+       } 
+    }
     render() {
         let photos = this.props.images
-        return (
-            <View style={{ flexDirection: 'row', flexWrap: 'wrap', left: 3}}>
-                {photos.map((a, i) => {
-                    return (
-                        <View key={i} style={{marginTop: 15, marginLeft:2, marginRight: 2}}>
-                            <TouchableHighlight onPress={this.props._detailsView}>
-                            <Image style={{height: 105, width: 190}} source={{uri: a.urls.regular }}/> 
-                            </TouchableHighlight>                       
-                        </View>
-                    )
-                }) }
-                
-            </View>
-
-        )
+            return (
+                <View style={this.state.gridSize}>
+                    {photos.map((a, i) => {
+                        return (
+                            <View key={i} style={{marginTop: 15, marginLeft:2, marginRight: 2}}>
+                                <TouchableHighlight onPress={this.props._detailsView}>
+                                <Image style={styles.container} source={{uri: a.urls.regular }}/> 
+                                </TouchableHighlight>                       
+                            </View>
+                        )
+                    }) }
+                    
+                </View>
+    
+            ) 
+        
+        
     }
 }
+
+
+const styles = StyleSheet.create({
+    container: {
+        height: height / 6.8,
+        width: width / 2.5
+    },
+    smallGrid:{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        left: 25,
+    },
+    largeGrid : {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        left: 90
+    }
+    
+})
